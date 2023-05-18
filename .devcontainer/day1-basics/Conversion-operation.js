9796d0d4f7f090d49b387fa46266485655f28d6e
tobecoder007 <133747156+tobecoder007@users.noreply.github.com>
2023-05-18 08:33:58 +0000

    conversion of datatypes

---
+++ .devcontainer/day1-basics/Conversion-operation.js
@@ -0,0 +1,91 @@
+// let score = 33
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let score = "33abc"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let score = true
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let score = false
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let score = NaN
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let score = null
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let score = undefined
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+// let isLoggedIn = 1
+
+// let booLeanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booLeanIsLoggedIn)
+
+// let isLoggedIn = "lavish"
+
+// let booLeanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booLeanIsLoggedIn)
+
+let isLoggedIn = ""
+
+let booLeanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booLeanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "lavish" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
