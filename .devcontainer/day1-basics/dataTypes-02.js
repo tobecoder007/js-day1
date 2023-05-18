a7419004d606e2771bc95dfef0e22329b3482963
tobecoder007 <133747156+tobecoder007@users.noreply.github.com>
2023-05-18 06:36:20 +0000

    datatypes

---
+++ .devcontainer/day1-basics/dataTypes-02.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS code as newer version 
+
+// alert( 3 + 3) result wont be declared as we are using nodejs, not browser
+
+
+let name = "Lavish"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigInt => used for cryptography 
+// string => "" (prefered) ''
+// boolean => true/false
+// null => standalone value { Typeof Object}
+// undefined => not defined yet
+// symbol => symbol is used to identify uniqueness
+
+// object => 
+
+console.log(typeof age);
+console.log(typeof undefined); // typeof Undefined 
+console.log(typeof null); // typeof object
