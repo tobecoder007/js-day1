bf28a9a98a5de182a08ba97e74b3d788706222ec
tobecoder007 <133747156+tobecoder007@users.noreply.github.com>
2023-05-18 09:34:59 +0000

    conversion

--- .devcontainer/day1-basics/Conversion-operation.js
+++ .devcontainer/day1-basics/Conversion-operation.js
@@ -75,17 +75,52 @@
 // let booLeanIsLoggedIn = Boolean(isLoggedIn)
 // console.log(booLeanIsLoggedIn)
 
-let isLoggedIn = ""
+// let isLoggedIn = ""
 
-let booLeanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booLeanIsLoggedIn)
+// let booLeanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booLeanIsLoggedIn)
 
-// 1 => true; 0 => false
-// "" => false
-// "lavish" => true
+// // 1 => true; 0 => false
+// // "" => false
+// // "lavish" => true
 
-let someNumber = 33
+// let someNumber = 33
 
-let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// let stringNumber = String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+// ******************************         operations *********************************************
+
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+// let str1 = "hello"
+// let str2 = " lavish"
+
+// let str3 = str1 + str2
+// console.log(str3)
+
+// Should avoid writing code like this 
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log(1 + 2 + "2");
+// console.log("1" + 2 + 2);
+
+// console.log((3 + 4) * 5 % 3);
+
+console.log(+true);
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter) 
