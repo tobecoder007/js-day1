2009ed5df093395f8e1cb721532cd69271ec8b54
tobecoder007 <133747156+tobecoder007@users.noreply.github.com>
2023-05-18 10:13:43 +0000

    comparision check done

---
+++ .devcontainer/day1-basics/comparision.js
@@ -0,0 +1,22 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// avoid such writing of code
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null <= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0); 
+
+// === // stands for triple equal which not only compares the value but also the datatype 
+
+console.log("2" === 2)
