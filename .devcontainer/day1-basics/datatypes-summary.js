c4d57e8f37b096e92694c38220917cae99818793
tobecoder007 <133747156+tobecoder007@users.noreply.github.com>
2023-05-18 13:22:36 +0000

    summary

---
+++ .devcontainer/day1-basics/datatypes-summary.js
@@ -0,0 +1,17 @@
+// primitive and non-primitive data ( how is data stored in memory and used to acess )
+
+// // Primitive data 
+// // 7 Types:
+// String
+// Number
+// Boolean
+// null
+// undefined
+// Symbol
+// BigInt
+
+// // Non-primitive or Reference type
+// Array
+// Objects
+// Functions
+
