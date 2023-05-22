5268fc73492c038e80543b7b9f244ce52b2470f8
tobecoder007 <133747156+tobecoder007@users.noreply.github.com>
2023-05-22 13:11:59 +0000

    dates & times

---
+++ .devcontainer/day1-basics/dates-times.js
@@ -0,0 +1,15 @@
+// Dates
+
+let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleDateString());
+// console.log(typeof myDate);
+
+// let myNewDate = new Date(2023, 0, 21, 5, 3, 1)
+// let myNewDate = new Date("2023-01-24");
+ let myNewDate = new Date("01-17-2023");
+
+console.log(myNewDate.toString());
