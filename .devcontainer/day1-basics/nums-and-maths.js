ab24252b876ca137a7f020066b8e10fe3b2bc301
tobecoder007 <133747156+tobecoder007@users.noreply.github.com>
2023-05-22 09:30:18 +0000

    mums push

---
+++ .devcontainer/day1-basics/nums-and-maths.js
@@ -0,0 +1,40 @@
+// const score = 400
+// const bankBalance = new Number(4000)
+// console.log(bankBalance);
+// console.log(typeof bankBalance);
+// console.log(typeof score)
+
+// console.log(bankBalance.toString().length);
+// console.log(bankBalance.toFixed(2));
+
+// const otherNumber = 32.8966
+// const anotherNumber = 1234
+
+// console.log(otherNumber.toPrecision(3));
+// console.log(anotherNumber.toPrecision(3));
+
+// const num = "420"
+// console.log(num == 420);
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-in'));
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ Maths ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.56));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(3.99));
+// console.log(Math.min(4, 3, 8, 9));
+// console.log(Math.max(4, 2, 1, 0));
+
+console.log(Math.random());
+console.log(Math.random()*10);
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1 );
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max - min + 1)) + min );
