1f17f57c21b71785dc29b3a8bc9ab2fa813985c1
tobecoder007 <133747156+tobecoder007@users.noreply.github.com>
2023-05-24 11:56:17 +0000

    array 1

---
+++ .devcontainer/day2-basics/array.js
@@ -0,0 +1,45 @@
+// array
+
+
+ const myArr = [0, 1, 2, 3, 4, 15]
+// const heros = ["shaktiman", "naagraj", "bahubali"]
+// const newArr = new Array(1, 2, 3, 4)
+
+// console.log(myArr)
+// console.log(myArr[1])
+// console.log(heros.length)
+// console.log(newArr)
+
+// Array Methods 
+
+// myArr.push(6)
+// myArr.push(9)
+// myArr.pop() 
+
+// myArr.unshift(10) 
+//  myArr.shift()
+
+// console.log(myArr.includes(3)); // question type
+// console.log(myArr.indexOf(12))
+// console.log(myArr.indexOf(15))
+// console.log(myArr);
+
+// const newArr = myArr.join()
+
+// console.log(myArr);
+// console.log(newArr);
+// console.log(typeof newArr);
+
+
+// Slice and Splice 
+
+console.log("A ", myArr);
+
+const myn1 = myArr.slice(1, 3)
+
+console.log(myn1);
+console.log("B ", myArr);
+
+const myn2 = myArr.splice(1, 3)
+console.log(myn2);
+console.log("C ", myArr);
