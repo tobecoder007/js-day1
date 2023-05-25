51c6a6db0f270538228d63739acdb7a26392c947
tobecoder007 <133747156+tobecoder007@users.noreply.github.com>
2023-05-25 13:46:40 +0000

    objects push4

--- .devcontainer/day2-basics/objects-4.js
+++ .devcontainer/day2-basics/objects-4.js
@@ -51,10 +51,28 @@ const users = [
 
 // console.log(users[1].id);
 
-console.log(tinderUser);
+// console.log(tinderUser);
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+const course = {
+    courseName: "js in hindi",
+    price: 999,
+    courseInstructor: "hitesh"
+}
+
+// console.log(course.courseInstructor); 
+
+const {courseInstructor: Inst} = course;
+
+// console.log(courseInstructor)
+console.log(Inst);
